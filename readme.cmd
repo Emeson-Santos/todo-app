42f4e5af510c2d6c0b6629b825d85d1b98c382df
esodegemeos <esodegemeos@gmail.com>
2022-04-19 09:34:28 -0300

    Adicionando a pasta backend inicial do projato

---
+++ readme.cmd
@@ -0,0 +1,8 @@
+# COMANDOS DO GIT
+echo "# todo-app" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Emeson-Santos/todo-app.git
+git push -u origin main
